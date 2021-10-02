c88e23144189a542d8b021cdd8e68819e314a467
Haruna-Kurahashi <kp.pickles.f.miffy@gmail.com>
2021-10-03 01:55:45 +0900

    add humberger menu

--- js/top.js
+++ js/top.js
@@ -5,3 +5,15 @@ const gradient = new Gradient();
 
 // Call `initGradient` with the selector to your canvas
 gradient.initGradient("#gradient-canvas");
+
+$(".openbtn").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $(".sp-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$(".link2").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass("active"); //ボタンの activeクラスを除去し
+  $(".sp-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+});
